0330eb0b86fd4afcfaf5ab35f0f11f1c73691d92
MichaelTB <mtbaker9817@gmail.com>
2015-08-16 11:48:37 -0400

    Adding onSelect and onClick functions
    Added on onSelect and onClick to the City Selection box to push the
    Selected value to the jobs.html page.

--- hirelyApp/www/app/home/homeCtrl.js
+++ hirelyApp/www/app/home/homeCtrl.js
@@ -17,5 +17,17 @@
 
       $scope.jobs = $firebaseArray(jobs);
 
- }
-})();
+      $scope.onSelect = function(place){
+	    var result = { selected :'place' };
+	    $state.go('app.job', result);
+	  }
+	  
+	  $scope.selectedJob = $scope.jobs[0];
+
+	  $scope.selectedCity = function(selected){
+      var result = { selectedCity :'selected' };
+      $state.go('app.job', result);
+};
+}
+    
+ })();
