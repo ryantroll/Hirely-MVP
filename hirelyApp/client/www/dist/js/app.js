8380580aad4577700bf1432e69fe7784137a4082
Zouhir Chahoud <zouhir.chahoud@servian.com.au>
2015-10-21 00:35:49 +1100

    Homepage redisign

--- hirelyApp/client/www/dist/js/app.js
+++ hirelyApp/client/www/dist/js/app.js
@@ -617,6 +617,53 @@ angular.module('hirelyApp.core')
  */
 
 /**
+ * Created by labrina.loving on 8/5/2015.
+ */
+
+(function() {
+    'use strict';
+
+    angular.module('hirelyApp.home', []);
+})();
+
+
+(function () {
+    'use strict';
+
+    angular.module('hirelyApp.home').controller('HomeCtrl', ['$scope', '$state', '$stateParams', 'GeocodeService', HomeCtrl ]);
+
+    function HomeCtrl ($scope, $state, $stateParams, GeocodeService, $window) {
+        var geocodeService = GeocodeService;
+
+        $scope.flexSliderOptions = {
+            animation: "fade",
+            directionNav: false,
+            controlNav: false,
+            slideshowSpeed: 10000
+        };
+
+        $scope.results = '';
+        $scope.options = {
+            types: '(regions)'
+        };
+        $scope.details = '';
+
+        var place = geocodeService.getPlace();
+        if(place){
+
+            $scope.results = place.formatted_address;
+            $scope.details = place;
+        }
+
+        $scope.getResults = function() {
+            geocodeService.setPlace($scope.details);
+            $state.go('app.job', {placeId: $scope.details.place_id})
+
+        };
+    }
+})();
+
+/**
  * Created by mike.baker on 8/10/2015.
  */
 
@@ -877,127 +924,6 @@ angular.module('hirelyApp.core')
 }
 
  })();
-
-/**
- * Created by labrina.loving on 8/5/2015.
- */
-
-(function() {
-    'use strict';
-
-    angular.module('hirelyApp.home', []);
-})();
-
-
-(function () {
-    'use strict';
-
-    angular.module('hirelyApp.home').controller('HomeCtrl', ['$scope', '$state', '$stateParams', 'GeocodeService', HomeCtrl ]);
-
-    function HomeCtrl ($scope, $state, $stateParams, GeocodeService, $window) {
-        var geocodeService = GeocodeService;
-
-        $scope.flexSliderOptions = {
-            animation: "fade",
-            directionNav: false,
-            controlNav: false,
-            slideshowSpeed: 10000
-        };
-
-        $scope.results = '';
-        $scope.options = {
-            types: '(regions)'
-        };
-        $scope.details = '';
-
-        var place = geocodeService.getPlace();
-        if(place){
-
-            $scope.results = place.formatted_address;
-            $scope.details = place;
-        }
-
-        $scope.getResults = function() {
-            geocodeService.setPlace($scope.details);
-            $state.go('app.job', {placeId: $scope.details.place_id})
-
-        };
-    }
-})();
-
-/**
- * Created by mike.baker on 8/17/2015.
- */
-
- (function () {
-    'use strict';
-
-    angular.module('hirelyApp.jobdetails').controller('JobDetailCtrl', ['$scope', '$state', '$stateParams','PositionService', 'GeocodeService', JobDetailCtrl ]);
-
-    function JobDetailCtrl ($scope, $state, $stateParams, PositionService, GeocodeService) {
-
-        var positionService = PositionService;
-        var geocodeService = GeocodeService;
-        var params = $stateParams;
-        var siteId = $stateParams.siteId;
-        var positionId = $stateParams.positionId;
-        var placeId = $stateParams.placeId;
-        $scope.position = '';
-        $scope.wageFormatted = '';
-        $scope.hoursFormatted = '';
-        $scope.distance = '';
-        $scope.photos = [];
-
-        positionService.getPositionbyId(siteId, positionId).then(function (positionObj) {
-            var today=new Date();
-            $scope.position = positionObj;
-            $scope.wageFormatted = positionObj.position.compensation.wage.maxAmount ? getMaxWageDisplay(positionObj.position.compensation.wage) : getnoMaxWageDisplay(positionObj.position.compensation.wage);
-            $scope.hoursFormatted =positionObj.position.workHours.max ? positionObj.position.workHours.min + '-' + positionObj.position.workHours.max : positionObj.position.workHours.min + '+'
-            var largePhoto = _.matcher({size: "l"});
-            var photos =  _.filter(positionObj.businessPhotos, largePhoto);
-            angular.forEach(photos, function(photoObj, photoKey) {
-
-                $scope.photos.push(photoObj.source);
-            });
-
-           geocodeService.calculateDistancetoSite(siteId, placeId).then(function (distance) {
-               $scope.distance = distance;
-           }, function (err) {
-                //TODO:  add error handling
-            });
-
-        }, function (err) {
-            //TODO:  add error handling
-        });
-
-        var getMaxWageDisplay = function(wage)
-        {
-
-            return  numeral(wage.minAmount).format('$0.00') + '-' + numeral(wage.maxAmount).format('$0.00');
-        }
-
-        var getnoMaxWageDisplay = function(wage)
-        {
-
-            return  numeral(wage.minAmount).format('$0.00') + '+';
-        }
-
-    }
-
-
-})();
-
- 
-/**
- * Created by mike.baker on 8/17/2015.
- */
-
-(function() {
-    'use strict';
-
-    angular.module('hirelyApp.jobdetails', []);
-})();
-
 
 /**
  * Created by labrina.loving on 8/6/2015.
@@ -1578,6 +1504,80 @@ angular.module('hirelyApp.core').directive('ngAutocomplete', ['GeocodeService', 
 
     angular.module('hirelyApp.shared', []);
 })();
+
+/**
+ * Created by mike.baker on 8/17/2015.
+ */
+
+ (function () {
+    'use strict';
+
+    angular.module('hirelyApp.jobdetails').controller('JobDetailCtrl', ['$scope', '$state', '$stateParams','PositionService', 'GeocodeService', JobDetailCtrl ]);
+
+    function JobDetailCtrl ($scope, $state, $stateParams, PositionService, GeocodeService) {
+
+        var positionService = PositionService;
+        var geocodeService = GeocodeService;
+        var params = $stateParams;
+        var siteId = $stateParams.siteId;
+        var positionId = $stateParams.positionId;
+        var placeId = $stateParams.placeId;
+        $scope.position = '';
+        $scope.wageFormatted = '';
+        $scope.hoursFormatted = '';
+        $scope.distance = '';
+        $scope.photos = [];
+
+        positionService.getPositionbyId(siteId, positionId).then(function (positionObj) {
+            var today=new Date();
+            $scope.position = positionObj;
+            $scope.wageFormatted = positionObj.position.compensation.wage.maxAmount ? getMaxWageDisplay(positionObj.position.compensation.wage) : getnoMaxWageDisplay(positionObj.position.compensation.wage);
+            $scope.hoursFormatted =positionObj.position.workHours.max ? positionObj.position.workHours.min + '-' + positionObj.position.workHours.max : positionObj.position.workHours.min + '+'
+            var largePhoto = _.matcher({size: "l"});
+            var photos =  _.filter(positionObj.businessPhotos, largePhoto);
+            angular.forEach(photos, function(photoObj, photoKey) {
+
+                $scope.photos.push(photoObj.source);
+            });
+
+           geocodeService.calculateDistancetoSite(siteId, placeId).then(function (distance) {
+               $scope.distance = distance;
+           }, function (err) {
+                //TODO:  add error handling
+            });
+
+        }, function (err) {
+            //TODO:  add error handling
+        });
+
+        var getMaxWageDisplay = function(wage)
+        {
+
+            return  numeral(wage.minAmount).format('$0.00') + '-' + numeral(wage.maxAmount).format('$0.00');
+        }
+
+        var getnoMaxWageDisplay = function(wage)
+        {
+
+            return  numeral(wage.minAmount).format('$0.00') + '+';
+        }
+
+    }
+
+
+})();
+
+ 
+/**
+ * Created by mike.baker on 8/17/2015.
+ */
+
+(function() {
+    'use strict';
+
+    angular.module('hirelyApp.jobdetails', []);
+})();
+
 
 /**
  * Created by labrina.loving on 9/6/2015.
