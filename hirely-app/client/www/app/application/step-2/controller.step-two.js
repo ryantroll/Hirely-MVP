2a72ae395b9a286185f586f6ce9de2865cb33dd6
Iyad Bitar <iyadbitar@Iyads-MacBook-Pro.local>
2016-01-22 15:10:34 -0500

    Work experience save with valiation, still WIP matching between occupation title and reported title

--- hirely-app/client/www/app/application/step-2/controller.step-two.js
+++ hirely-app/client/www/app/application/step-2/controller.step-two.js
@@ -7,29 +7,126 @@
 (function () {
   'use strict';
 
-  angular.module('hirelyApp').controller('StepTwoController', ['$scope', '$stateParams', 'GeocodeService', 'OccupationService', StepTwoController]);
+  var hirelyApp = angular.module('hirelyApp').controller('StepTwoController', ['$scope', '$stateParams', '$filter', 'GeocodeService', 'OccupationService', 'AuthService', 'UserService', StepTwoController]);
 
+  hirelyApp.directive('validateMonth', function(){
+    return {
+      restrict:'A',
+      require:'ngModel',
+      link:function(scope, ele, attrs, ctrl){
+        ctrl.$parsers.unshift(function(value){
+          var m = Number(value);
+          var valid = undefined !== m && NaN !== m && m < 13 && m > 0;
+          ctrl.$setValidity('invalidMonth', valid)
 
-  function StepTwoController($scope, $stateParams, GeocodeService, OccupationService) {
+          return valid ? value : undefined;
+        });/// unshift
+      }//// fun. link
+    }/// return object
+
+  })/// validate month
+  .directive('validateYear', function(){
+    return {
+      restrict:'A',
+      require:'ngModel',
+      link:function(scope, ele, attrs, ctrl){
+        ctrl.$parsers.unshift(function(value){
+          var m = Number(value);
+          var now = new Date();
+          var valid = undefined !== m && NaN != m && m <= now.getFullYear();
+          ctrl.$setValidity('invalidYear', valid);
+
+          return valid ? value : undefined;
+        });/// unshift
+      }//// fun. link
+    }/// return object
+
+  })/// validate month
+
+  function StepTwoController($scope, $stateParams, $filter, GeocodeService, OccupationService, AuthService, UserService) {
 
     var geocodeService = GeocodeService;
+    var orderBy = $filter('orderBy');
 
+    $scope.stepTwoLoaded = false;
 
+    /**
+     * [xpItems will hold the work experience data]
+     * @type {Array}
+     */
     $scope.xpItems = [];
-    $scope.addJobXp = function () {
-      console.log($scope.company);
-      $scope.xpItems.push(
-        {
-          company: $scope.company,
-          position: $scope.position,
-          description: $scope.description
+
+    UserService.getUserCompleteFields(AuthService.currentUserID, ['workExperience'])
+    .then(
+      function(founded){
+        if(Array.isArray(founded.workExperience)){
+          /**
+           * Work experience is founded
+           */
+          angular.forEach(founded.workExperience, function(item){
+            /**
+             * do some dates clenaing and fixing
+             */
+            item.dateStart = new Date(item.dateStart);
+            item.dateEnd = new Date(item.dateEnd);
+            item.dateStartYear = item.dateStart.getFullYear();
+            item.dateStartMonth = item.dateStart.getMonth() + 1;
+            if(!isNaN(item.dateEnd.getTime()) ){
+              item.dateEndYear = item.dateEnd.getFullYear();
+              item.dateEndMonth = item.dateEnd.getMonth() + 1;
+            }
+          });
+          $scope.xpItems = orderBy(founded.workExperience, 'dateStart', true);
+
         }
-      )
+        else{
+
+        }
+        $scope.stepTwoLoaded = true;
+      },
+      function(err){
+        //// console.log(err);
+        $scope.stepTwoLoaded = true;
+      }
+    );//// then
+
+
+
+    /**
+     * [addJobXp will add new work experience object to array after setting dataStart and dataEnd]
+     */
+    $scope.addJobXp = function () {
+      if(!$scope.stepTwo.$valid) return null;
+
+      var newExp = angular.copy($scope.occupation);
+      newExp.dateStart = new Date(Number(newExp.dateStartYear), Number(newExp.dateStartMonth)-1, 1);
+
+      if(false == newExp.currentlyHere){
+        newExp.dateEnd = new Date(Number(newExp.dateEndYear), Number(newExp.dateEndMonth)-1, 1);
+      }
+
+      $scope.xpItems.push(newExp);
+
+      $scope.xpItems = orderBy($scope.xpItems, 'dateStart', true)
+    }//// fun. addJobXp
+
+    /**
+     * [removeJobXp will remove work wperience from the array]
+     * @param  {number} index [index of item to be removed]
+     * @return {null}       [description]
+     */
+    $scope.removeJobXp = function(index){
+      $scope.xpItems.splice(index, 1);
     }
 
       var locations = [];
       $scope.selectedLocation = undefined;
 
+      /**
+       * [searchLocations used for the typeahead in company name, will get the matchin addrss based on entered query]
+       * @param  {string} query [text string to search google places for]
+       * @return {array}       [array of address objects from google places]
+       */
       $scope.searchLocations = function(query){
           if(!!query && query.trim() != ''){
 
@@ -55,6 +152,10 @@
           }//// if query
       };/// fun. searchLocations
 
+      /**
+       * [setAddress will set the right address component in scop when user select one address form typeahead]
+       * @param {object} address [address object from selected item in typeahead]
+       */
       $scope.setAddress = function(address){
           $scope.occupation.formattedAddress = address.address;
           $scope.occupation.googlePlaceId = address.placeId;
@@ -102,6 +203,11 @@
 
       }//// fun. setAddress
 
+      /**
+       * [searchPosition will be used in typeahead to query onet positions and return a list of matching position]
+       * @param  {string} query [string from typeahead text field]
+       * @return {array}       [array of matched position form onet api]
+       */
       $scope.searchPosition = function(query){
         if(query.trim().length > 1){
           return OccupationService.getOccupations(query)
@@ -127,10 +233,116 @@
         }
       }//// fun. searchPositions
 
+      /**
+       * [setPosition used in typeahead and triggered when user select one of the position in typeahead list]
+       * @param {Object} position [description]
+       */
       $scope.setPosition = function(position){
         $scope.occupation.reportedJobName = position.title;
-        $scope.occupation.onetOccupationId
+        $scope.occupation.onetOccupationId = position.code;
       }
+
+      /**
+       * Watch dates to make sure end data is greater than start date
+       */
+       $scope.$watchCollection('[occupation.dateStartYear, occupation.dateStartMonth, occupation.dateEndYear, occupation.dateEndMonth]', function(newValues, oldVaues){
+          var start = new Date(Number(newValues[0]), Number(newValues[1])-1, 1);
+          var end = new Date(Number(newValues[2]), Number(newValues[3])-1, 1);
+
+          /**
+           * check if end date is less than start date
+           */
+          if( !isNaN(start.getTime() ) && !isNaN(end.getTime() ) ){
+            $scope.stepTwo.workDateEndY.$setValidity('invalidEndDate', end > start);
+            $scope.stepTwo.workDateEndM.$setValidity('invalidEndDate', end > start);
+          }
+
+          /**
+           * Check for date conflicts with previous dates
+           */
+          if(Array.isArray($scope.xpItems))
+          for(var x=0; x<$scope.xpItems.length; x++){
+            /**
+             * check if start date in a middle of range
+             */
+            if(start >= $scope.xpItems[x].dateStart && start < $scope.xpItems[x].dateEnd){
+              $scope.stepTwo.workDateStartY.$setValidity('startDateConflict', false);
+              $scope.stepTwo.workDateStartM.$setValidity('startDateConflict', false);
+              $scope.xpItems[x].conflict = true;
+              break;
+            }
+            else{
+              $scope.stepTwo.workDateStartY.$setValidity('startDateConflict', true);
+              $scope.stepTwo.workDateStartM.$setValidity('startDateConflict', true);
+              $scope.xpItems[x].conflict = false;
+            }
+
+            /**
+             * check if end date in a middle of range
+             */
+            if(end > $scope.xpItems[x].dateStart && end <= $scope.xpItems[x].dateEnd){
+              $scope.stepTwo.workDateEndY.$setValidity('endDateConflict', false);
+              $scope.stepTwo.workDateEndM.$setValidity('endDateConflict', false);
+              $scope.xpItems[x].conflict = true;
+              break;
+            }
+            else{
+              $scope.stepTwo.workDateEndY.$setValidity('endDateConflict', true);
+              $scope.stepTwo.workDateEndM.$setValidity('endDateConflict', true);
+              $scope.xpItems[x].conflict = false;
+            }
+
+            /**
+             * Check if new rang is overlap one old rag
+             */
+            if(start <= $scope.xpItems[x].dateStart && end >= $scope.xpItems[x].dateEnd){
+              $scope.stepTwo.workDateEndY.$setValidity('endDateConflict', false);
+              $scope.stepTwo.workDateEndM.$setValidity('endDateConflict', false);
+              $scope.stepTwo.workDateStartY.$setValidity('startDateConflict', false);
+              $scope.stepTwo.workDateStartM.$setValidity('startDateConflict', false);
+              $scope.xpItems[x].conflict = true;
+              break;
+            }
+            else{
+              $scope.stepTwo.workDateEndY.$setValidity('endDateConflict', true);
+              $scope.stepTwo.workDateEndM.$setValidity('endDateConflict', true);
+              $scope.stepTwo.workDateStartY.$setValidity('startDateConflict', true);
+              $scope.stepTwo.workDateStartM.$setValidity('startDateConflict', true);
+              $scope.xpItems[x].conflict = false;
+            }
+
+          }//// for
+       });//// $watch
+
+      $scope.$on('$destroy', function(){
+        /**
+         * Make sure user is authenticated
+         */
+        AuthService.getAuth()
+        .then(
+          function(isAuth){
+            /**
+             * User is authenticated
+             * do save
+             */
+            if(isAuth){
+              var toSave = {workExperience:angular.copy($scope.xpItems)};
+              UserService.saveUser(toSave, AuthService.currentUserID)
+              .then(
+                function(user){
+                  // console.log(user);
+                },
+                function(err){
+                  alert(err);
+                }
+              )
+            }//// if isAuth
+            else{
+
+            }//// if isAuth else
+          }//// reslove
+        )//// getAuth.then()
+      })/// $on.$destroy
 
   }
 })();
