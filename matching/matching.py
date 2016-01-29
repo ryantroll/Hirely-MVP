0704693f7b03bf3062b8e7e15ede4cab7845fdf0
BrianDombrowski <bdombro@gmail.com>
2016-01-28 22:08:26 -0500

    Snapshot

--- matching/matching.py
+++ matching/matching.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-class UserService(object):
+# TODO: EducationScores is a to be created lookup table for pre-calculated education percentiles for a given occupation
+# TODO: Application scores should be generated when an application is submitted to avoid crunching multiple times
+
+class UserService:
   experienceLevels =  [0, 3, 6, 12, 24, 48, 64, 98, 124] # tiers of experience where level = num of months
 
   def monthCountToExperienceLevel(monthCount):
@@ -79,7 +82,100 @@ class UserService(object):
     return user
 
 
-class MatchCacheService(object):
+class BusinessService:
+
+  def qualificationComputer(self, operator, left, right):
+    switch(operator) {
+      case "+":
+        return left + right;
+        break;
+      case "-":
+        return left - right;
+        break;
+      case "/":
+        return left / right;
+      case "*":
+        return left * right;
+      case "%":
+        return left % right;
+      case "^":
+        return Math.pow(left, right);
+      case "within":
+        for v in left:
+          if right.indexOf(v) == -1:
+            return 0
+        return 1
+    }
+
+
+  def qualificationFormulaEvaluator(self, formula, business, location, position, variant, user):
+    objLookup = {
+      'business': business,
+      'location': location,
+      'position': position,
+      'variant': variant,
+      'user': user
+    }
+    
+    result = 0
+    
+    if formula['type'] in ['number', 'string']:
+      result = formula['value']
+    
+    if formula['type'] == 'attribute':
+      parts = formula['value'].split('.')
+      obj = objLookup[parts.pop(0)]
+      result = obj
+      for part in parts:
+        result = result[part]
+    
+    elif formula['type'] == 'computation':
+      result = self.qualificationFormulaEvaluator(formula['operands'].pop())
+      for childField in formula['operands']:
+        # TODO:  create compute function.  Should not use eval
+        result = self.qualificationComputer(formula['operator']), result, self.qualificationFormulaEvaluator(formula.pop('operands'))
+    
+    return result
+
+  def getQualificationScore(self, variantId, userId, disqualifyThreshold=0):
+    user = User.findOne(id=userId)
+    business = Business.findOne(variants__id=variandId)
+    variant = business['variants'][variantId]
+    position = business['position'][variant['positionId']]
+    location = business['locations'][variant['locationId']]
+    
+    fScoreSum = 0
+    fScoreMaxSum = 0
+    disqualified = false
+    for formula in variant['qualificationSpecification']['formulas']:
+      fScoreSum = self.qualificationFormulaEvaluator(formula=formula, 
+                                                     business=business, 
+                                                     location=location, 
+                                                     position=position, 
+                                                     variant=variant, 
+                                                     user=user)
+      if fScore > 0:
+        fScore = 1
+      
+      fScoreMaxSum += formula['importance']
+      fScoreSum += fScore * formula['importance']
+    
+      if fScore == 0 and formula['importance'] >= disqualifyThreshold:
+        disqualified = true
+    
+    if disqualified:
+      qScore = 0
+    else if fScoreMaxSum == 0:
+      qScore = 1
+    else:
+      qScore = qScore / maxScore
+    return qScore
+
+  # This should be called when a variant's specification 
+  def updateApplicationScores
+
+
+class MatchCacheService:
   # I have this list, when you are ready
   onetOccupationIds = [...]
   experienceLevels =  [0, 3, 6, 12, 24, 48, 64, 98, 124] # tiers of experience where level = num of months 
@@ -137,7 +233,6 @@ class MatchCacheService(object):
         matchCache.scores[experienceLvl]['experienceScore'] = experienceScore
       
         # Education calcs
-        # TODO: EducationScores is a to be created lookup table for pre-calculated education percentiles for a given occupation
         matchCache.scores[experienceLvl]['educationScore'] = OnetMeta.findOne(onetOccupationId=onetOccupationId)['percentiles'][experienceLvl]['educationIndex'][user.educationMaxLvl]
 
         # Personality calcs
@@ -167,23 +262,20 @@ class MatchCacheService(object):
 
     # Get all businesses with open position variants within a lat-lon region
     if lng_lower_bound and lng_upper_bound and lat_lower_bound and lat_upper_bound:
-      Business.find(locations__positions__variants__openings__gt=0, 
+      Business.find(variants__openings__gt=0, 
                     limit=limit,
                     locations__lng__within=(lng_lower_bound, lng_upper_bound), 
                     locations__lat__within=(lat_lower_bound, lat_upper_bound))
     else:
-      Business.find(locations__positions__variants__openings__gt=0, 
+      Business.find(variants__openings__gt=0, 
                     limit=limit)
     
     # Now get match scores for every variant and append to suggestions
-    for business in business['locations']:
-      for location in business['locations']:
-        for position in location['positions']:
-          for variant in position['variants']:
-            if variant.openings > 0:
-              matchCache = MatchCache.findOne(onetOccupationId=variant.onetOccupationId, userId=userId)
-              scores = matchCache.scores[variant.experienceLvl]
-              suggestions.append({"businessId": business.id, "variantId": variant.id, "scores": scores})
+    for variant in business['variants']:
+      if variant.openings > 0:
+        matchCache = MatchCache.findOne(onetOccupationId=variant.onetOccupationId, userId=userId)
+        scores = matchCache.scores[variant.experienceLvl]
+        suggestions.append({"business": business, "variant": variant, "scores": scores})
 
     # Finally, sort by suggestion['scores']['overallScore'] and limit to top 100
     return suggestions.sort('scores.overallScore')[:100]
@@ -200,28 +292,53 @@ class MatchCacheService(object):
       queryset = MatchCache.find(onetOccupationId=onetOccupationId,
                                  userOptOutOfSuggetionsFromEmployers=False, 
                                  sort_by_descending=scores[experienceLvl]['overallScore'], limit=100)
+
+      for index, matchCatch in queryset:
+        user = User.findOne(id=matchCache['userId'])
+        # TODO:  Strip out irrelevant user fields
+        queryset[index]['user'] = user
     return queryset
 
 
-class ApplicationService
+class ApplicationService:
+  # This should be an api endpoint
+  def getApplicationsByBusiness(businessId):
+    return Applications.find(businessId=businessId)
+
+  # This should be an api endpoint
+  def getApplicationsByLocation(locationId):
+    return Applications.find(locationId=locationId)
+
+  # This should be an api endpoint
+  def getApplicationsByPosition(positionId):
+    return Applications.find(positionId=positionId)
+
   # This should be an api endpoint
   def getApplicationsByVariant(variantId):
     return Applications.find(variantId=variantId)
 
   # This should be an api endpoint
-  def createApplication(businessId, variantId, userId, prescreenAnswers):
-    business = Business.findOne(businessId=businessId)
+  def createApplication(variantId, userId, prescreenAnswers):
+    business = Business.findOne(business__variant=variantId)
     user = User.findOne(userId=userId)
 
-    application = new Application
+    application = new Application()
 
-    # Now get match scores for every variant and append to suggestions
-    for location in business['locations']:
-      for position in location['positions']:
-        for variant in position['variants']:
-          if variant.id == variantId:
-            matchCache = MatchCache.findOne(onetOccupationId=variant.onetOccupationId, userId=userId)
-            application.scores = matchCache.scores[variant.experienceLvl]
+    variant = business['variants'][variantId]:
+    matchCache = MatchCache.findOne(onetOccupationId=variant.onetOccupationId, userId=userId)
+    application.scores = matchCache.scores[variant.experienceLvl]
+    application.scores['experienceScore'] *= variant['scoreWeights']['experience']
+    application.scores['educationScore'] *= variant['scoreWeights']['education']
+    application.scores['personalityScore'] *= variant['scoreWeights']['personality']
+    
+    # TODO:  Finish QualificationSpecificationService
+    application.scores['qualificationScore'] = qualificationSpecificationService.getQaulificationScore(variantId=variant.id, userId=user.id, disqualifyThreshold=0)
+    application.scores['qualificationScore'] *= variant['scoreWeights']['qualification']
+    
+    application.scores['overallScore'] = avg(application.scores['experienceScore'],
+                                             application.scores['educationScore'],
+                                             application.scores['personalityScore'],
+                                             application.scores['qualificationScore'])
 
     application.save()
 
@@ -236,20 +353,20 @@ def test():def test():
 
   # job is some onetOccupation, and the scores are based on x months experience
   business = Business.findOne()
-  l = business.locations[0]
-  v = l.positions[0].variants[0]
+  l = business['locations'].first()
+  v = l['positions'].first()['variants'].first()
 
   userSuggestions = businessService.getUserSuggestions(onetOccupationId=v.onetOccupationId, 
                                                        experienceLvl=v.experienceLvl, 
-                                                       lng_lower_bound=(l.address.lng-.5), 
-                                                       lng_upper_bound=(l.address.lng+.5), 
-                                                       lat_lower_bound=(l.address.lat-.5), 
-                                                       lat_upper_bound=(l.address.lat+.5))
+                                                       lng_lower_bound=(l['address']['lng']-.5), 
+                                                       lng_upper_bound=(l['address']['lng']+.5), 
+                                                       lat_lower_bound=(l['address']['lat']-.5), 
+                                                       lat_upper_bound=(l['address']['lat']+.5))
 
   careerSuggestions = userService.getCareerSuggestions(user=user)
 
   variantSuggestions = userService.getVariantSuggestions(user=user,
-                                                         lng_lower_bound=(user.address.lng-.5), 
-                                                         lng_upper_bound=(user.address.lng+.5), 
-                                                         lat_lower_bound=(user.address.lat-.5), 
-                                                         lat_upper_bound=(user.address.lat+.5))
+                                                         lng_lower_bound=(user['address']['lng']-.5), 
+                                                         lng_upper_bound=(user['address']['lng']+.5), 
+                                                         lat_lower_bound=(user['address']['lat']-.5), 
+                                                         lat_upper_bound=(user['address']['lat']+.5))
