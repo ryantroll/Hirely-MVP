a7d0fdda61c182ae85c1a53b3b66857d0536660e
Iyad Bitar <iyadbitar@Iyads-MacBook-Pro.local>
2016-02-10 14:29:36 -0500

    New wireframe completed for user profile | availability step done

--- hirely-app/client/www/app/user/profile/availability/controller.availability.js
+++ hirely-app/client/www/app/user/profile/availability/controller.availability.js
@@ -18,9 +18,9 @@
    * Controller Definition ********************************************************
    * ******************************************************************************
    */
-  step5App.controller('ProfileAvailabilityController', ['$scope', '$stateParams', '$window', 'multiStepFormInstance', 'GeocodeService', '$q', 'AvailabilityService', 'AuthService', ProfileAvailabilityController])
+  step5App.controller('ProfileAvailabilityController', ['$scope', '$stateParams', '$filter', 'multiStepFormInstance', 'GeocodeService', '$q', 'AvailabilityService', 'AuthService', ProfileAvailabilityController])
 
-  function ProfileAvailabilityController($scope, $stateParams, $window, multiStepFormInstance, GeocodeService, $q, AvailabilityService, AuthService) {
+  function ProfileAvailabilityController($scope, $stateParams, $filter, multiStepFormInstance, GeocodeService, $q, AvailabilityService, AuthService) {
 
     /**
      * [availability this object will hold the data that need bot saved in database
@@ -34,16 +34,24 @@
 
     $scope.stepFiveLoaded = false;
 
-    $scope.startOptions = [
-      {days:0, label:'Immediatly'},
-      {days:1, label:'1 Day'},
-      {days:2, label:'2 Days'},
-      {days:3, label:'3 Days'},
-      {days:4, label:'4 Days'},
-      {days:5, label:'5 Days'},
-      {days:7, label:'1 Week'},
-      {days:14, label:'2 Weeks'},
-    ];
+    $scope.startOptions = AvailabilityService.startOptions;
+
+    $scope.days = AvailabilityService.days;
+
+    $scope.hours = AvailabilityService.hours;
+
+    /**
+     * [dayHours will hold the numbe of hours in each day]
+     * @type {Object}
+     */
+    $scope.dayHours = {};
+
+    /**
+     * [totalHours will hold the total number of hours in a week and will be calculated on timetable save and initial]
+     * @type {Number}
+     */
+    $scope.totalHours = 0;
+
 
       /**
        * [weeklyTimetable build the weekly timetable for availability and assign it to scope
@@ -62,22 +70,33 @@
           function(retObj){
             //// the availability exists in DB
             $scope.availability = retObj;
+
+            //// set the number of hours in each day
+            $scope.totalHours = 0;
+            for(var d=0; d<7; d++){
+              var dayHours = AvailabilityService.getDayHours($scope.availability.weeklyTimetable[$scope.days[d]]);
+              $scope.dayHours[$scope.days[d]] = dayHours;
+              $scope.totalHours += dayHours;
+            }
+
           },/// resolve
           function(){
             //// availability not in DB create empty one of user
-            var weeklyTimetable = [];
-            for(var h=0; h<24; h++){
-              var days = {};
-              for(var day in AvailabilityService.days){
-                days[AvailabilityService.days[day]] = false;
-              }/// for in
-              weeklyTimetable[h] = {
-                'label': AvailabilityService.hours[h].label,
-                'days': days
-              };
-            }//// for
-
+            var weeklyTimetable = {};
+            for(var d=0; d<7; d++){
+              weeklyTimetable[$scope.days[d]] = [];
+              for(var h=0; h<24; h++){
+                var _h = angular.extend({}, $scope.hours[h]);
+                _h.active = false;
+                weeklyTimetable[$scope.days[d]].push(_h);
+              }//// for h
+            }//// for d
             $scope.availability.weeklyTimetable = weeklyTimetable;
+
+            //// build empty dayHours
+            for(var d=0; d<7; d++){
+              $scope.dayHours[$scope.days[d]] = 0;
+            }
 
           }//// reject
         )//// then isAvailabilityExists
@@ -101,41 +120,13 @@
       function initializeScope(){
 
         /**
-         * [initialize the maxHour and minHours variables ]
-         * @type {Number}
-         */
-        if(angular.isUndefined($scope.availability.hoursPerWeekMax)) $scope.availability.hoursPerWeekMax = 1;
-        if(angular.isUndefined($scope.availability.hoursPerWeekMin)) $scope.availability.hoursPerWeekMin = 1;
-        if(angular.isUndefined($scope.availability.seekerStatus)) $scope.availability.seekerStatus = true;
-        if(angular.isUndefined($scope.availability.startAvailability)) $scope.availability.startAvailability = 0;
-
-        /**
-         * [weeklyRanges scope variable to hold the the range data for mobile layout only
-         * the a temp variable is used to get the ranges from AvailabilityServices]
-         * @type {Object}
-         */
-
-        var ranges = {};
-        ranges = AvailabilityService.updateRanges($scope.availability.weeklyTimetable);
-        $scope.weeklyRanges = ranges;
-
-
-        /**
-         * [totalHours hold the total number of hours in each week and each day
-         * with .total property for total in week, .sa property total in sunday, ..
-         * used for validation and display purpos only]
-         * @type {[object]}
-         */
-        $scope.totalHours = AvailabilityService.getTotalHours($scope.availability.weeklyTimetable);
-
-        /**
          * Need to wait untill all views and data is been loaded to update the validity of form
          * because the multi step form doesn't provide any way to detect the step loading we have to use $q.all
          */
         $q.all()
           .then(function() {
             //At this point all data is available for angular to render
-            $scope.updateValidity();
+            // $scope.updateValidity();
 
             /**
              * [stepFiveLoaded set to true to remove loader and show form]
@@ -154,177 +145,110 @@
        * @param  {[number]} hour [hour of day to be set 0 -> 23]
        * @return {[type]}      [description]
        */
-      $scope.hourClick = function(day, hour){
+      $scope.hourClick = function(hour){
 
-        $scope.availability.weeklyTimetable[hour].days[day] = !$scope.availability.weeklyTimetable[hour].days[day];
+        if(angular.isUndefined($scope.currentDays)) return null;
 
-        //// update totalHours
-        $scope.totalHours = AvailabilityService.getTotalHours($scope.availability.weeklyTimetable);
+        $scope.currentDays[$scope.currentDayLabel][hour].active = !$scope.currentDays[$scope.currentDayLabel][hour].active
 
-        $scope.updateValidity();
       }//// fun. hourClick
 
       $scope.updateValidity = function(){
         //// set validity for max and min hours
         // $scope.stepFive.maxHours.$setValidity( 'mismatch', $scope.totalHours.total <= $scope.availability.hoursPerWeekMax);
         // console.log($scope.totalHours.total, $scope.availability.hoursPerWeekMin)
-        $scope.stepFive.minHours.$setValidity( 'mismatch', $scope.totalHours.total >= $scope.availability.hoursPerWeekMin);
+        $scope.stepFive.minHours.$setValidity( 'mismatch', $scope.totalHours >= $scope.availability.hoursPerWeekMin);
         $scope.stepFive.maxHours.$setValidity( 'mismatch', $scope.availability.hoursPerWeekMin <= $scope.availability.hoursPerWeekMax);
       }
 
-
       /**
-       * [isMobile will be set on window.resize event]
-       * @type {Boolean}
-       */
-      $scope.isMobile = false;
-
+     * [fixFormDiv will set the form div to window height and scroll page to top
+     * form is shown as an overlay and should cover the whole screen]
+     * @return {null}
+     */
+    function fixFormDiv(){
+      var formDiv = $('#expFormDiv');
+      $(window).scrollTop(0);
       /**
-       * [onResize window.resize event to detect screen width
-       * and trigger availablility ranges excraction from timetable when on mobile
-       * availablity ranges need only on mobile so we update them only when on mobile]
-       * @return {[type]} [description]
+       * Add some delay so we can read the height property after div is added to dom
        */
-      $scope.onResize = function(){
-        // $scope.isMobile = $window.innerWidth <= 768;
-        if($window.innerWidth <= 768 && !$scope.isMobile){
-          $scope.isMobile = true;
-          var ranges = {};
-          ranges = AvailabilityService.updateRanges($scope.availability.weeklyTimetable);
-          $scope.weeklyRanges = ranges;
+      setTimeout(function(){
+        if(formDiv.height() < $(document).height()){
+          formDiv.height($(document).height());
         }
-        else{
-          $scope.isMobile = false;
+      },100)
+    }
+
+      $scope.showTimeTable = function(day, index){
+
+        /**
+         * Don't create new copy of currentDays if exists
+         * this function can be called from inside days buttons in this case the other days data will be erased if copy is exectured
+         * Only save and cancel will delete currentDays
+         */
+        if(angular.isUndefined($scope.currentDays)){
+          $scope.currentDays = angular.copy($scope.availability.weeklyTimetable);
         }
-      }//// fun. onResize
+        $scope.currentDayLabel = day;
+        $scope.currentDayIndex = index;
 
-      /**
-       * [window resize event binding, it calls scope function only]
-       *
-       */
-      angular.element($window).bind('resize', function(){
-        $scope.onResize();
-      });
+        $scope.addTimetable = true;
 
-      /**
-       * [removeRange trigger on click event of 'delete' button next to each time availablity range on mobile]
-       * @param  {[string]} hashKey [angular hashke value to ]
-       * @param  {[string]} day     [name of day in short format]
-       * @param  {[number]} start   [hour to start from]
-       * @param  {[number]} count   [hour to end on]
-       * @return {[type]}         [description]
-       */
-      $scope.removeRange = function(hashKey, day, start, count){
+        fixFormDiv();
+      }//// showTimetable;
 
-        //// remove from timetable
-        for(var i=start; i<start+count && i < 24; i++){
-          $scope.availability.weeklyTimetable[i].days[day] = false;
+      $scope.cancelTimetable = function(){
+        delete $scope.currentDays;
+        delete $scope.currentDayLabel;
+        delete $scope.currentDayIndex;
+
+        $scope.addTimetable = false;
+      }/// fun. cancelTimetable
+
+      $scope.saveTimetable = function(){
+        if(angular.isUndefined($scope.currentDays) || angular.isUndefined($scope.currentDayLabel)){
+          return null;
         }
 
-        ///// remove from ranges
-        ///// No need to rebuild ranges array
-        ///// we can remove the range directly for quicker update
-        angular.forEach($scope.weeklyRanges[day], function(obj, index){
-          if(obj.$$hashKey === hashKey){
-            $scope.weeklyRanges[day].splice(index, 1);
-          }
-        });
+        $scope.availability.weeklyTimetable = angular.copy($scope.currentDays);
 
-        //// update totalHours
-        $scope.totalHours = AvailabilityService.getTotalHours($scope.availability.weeklyTimetable);
-
-        //// update the validity
-        $scope.updateValidity();
-      }/// fun. removeRange
-
-
-      /**
-       * [formRanges  will hold the selected values when user input availability on mobile
-       * this variable and its value only for form input and shouldn't be saved in DB]
-       * @type {Object}
-       */
-      $scope.formRanges = {};
-      for(var d = 0; d<7; d++){
-        $scope.formRanges[ AvailabilityService.days[d] ] = {'min': undefined, 'max': undefined};
-      }
-
-      /**
-       * [hoursList to show list of hours in 'From' drop down in mobile version
-       * Slice is used to brack the object reference and get fresh one]
-       * @type {[type]}
-       */
-      $scope.hoursListMin =  AvailabilityService.hours.slice(0);
-
-      /**
-       * [hoursListMax to show list of hours in 'From' drop down in mobile version
-       * Slice is used to brack the object reference and get fresh one
-       * the first item in array which is 12AM is rotated to the end of array
-       * to allow user to end his range on 12AM]
-       * @type {[type]}
-       */
-      $scope.hoursListMax =  AvailabilityService.hours.slice(0);
-      $scope.hoursListMax.push($scope.hoursListMax.shift());
-
-      /**
-       * [rangeMinChange triggered on change event of 'from' drop-down to prevent user from selecting wrong range boundray]
-       * @param  {[string]} day [name of day in short format]
-       * @return {[none]}     [description]
-       */
-      $scope.rangeMinChange = function(day){
-        if(!angular.isUndefined($scope.formRanges[day].max)){
-          var start = parseInt( $scope.formRanges[day].min,10);
-          var end = parseInt($scope.formRanges[day].max,10);
-
-          if(start >= end && end !== 0) $scope.formRanges[day].max = undefined;
-        }
-      }
-
-      /**
-       * [rangeMaxChange triggered on change event of 'to' drop-down to prevent user from selecting wrong range boundray]
-       * @param  {[string]} day [name of day in short format]
-       * @return {[type]}     [description]
-       */
-      $scope.rangeMaxChange = function(day){
-        if(!angular.isUndefined($scope.formRanges[day].min)){
-          var start = parseInt( $scope.formRanges[day].min,10);
-          var end = parseInt($scope.formRanges[day].max,10);
-          if(start >= end && end !== 0) $scope.formRanges[day].min = undefined;
-        }
-      }
-
-      /**
-       * [addRange triggered on click event of 'add' button to add new availability range on mobile]
-       * @param {[string]} day [name of day in short format]
-       */
-      $scope.addRange = function(day){
-        if(!angular.isUndefined($scope.formRanges[day].min)
-          && !angular.isUndefined($scope.formRanges[day].max)
-        ){
-          var start = parseInt($scope.formRanges[day].min, 10);
-          var end = parseInt($scope.formRanges[day].max, 10);
-
-          //// adjust end of range when user select 12AM in range max
-          if(end === 0){
-            end = 24;
+        /**
+         * Update the number of hours for each day
+         */
+         $scope.totalHours = 0;
+          for(var d=0; d<7; d++){
+            var dayHours = AvailabilityService.getDayHours($scope.availability.weeklyTimetable[$scope.days[d]]);
+            $scope.dayHours[$scope.days[d]] = dayHours;
+            $scope.totalHours += dayHours;
           }
 
-          if(start < end){
-            //// Add to timetable
-            for(var i=start; i<end && i < 24; i++){
-              $scope.availability.weeklyTimetable[i].days[day] = true;
-            }
+          $scope.updateValidity();
 
-            $scope.formRanges[day].min = undefined;
-            $scope.formRanges[day].max = undefined;
+        delete $scope.currentDays;
+        delete $scope.currentDayLabel;
+        delete $scope.currentDayIndex;
 
-            var ranges = {};
-            ranges = AvailabilityService.updateRanges($scope.availability.weeklyTimetable);
-            $scope.weeklyRanges = ranges;
-          }/// if start < end
-        }/// if !isUndefined
-      }//// fun. addRange
+        $scope.addTimetable = false;
+      }//// fun. saveTimetable
 
+      $scope.nextDay = function(){
+        if(angular.isUndefined($scope.currentDays) || angular.isUndefined($scope.currentDayLabel) || angular.isUndefined($scope.currentDayIndex)){
+          return null;
+        }
+        $scope.currentDayIndex = ($scope.currentDayIndex + 1) % 7;
+        $scope.currentDayLabel = $scope.days[$scope.currentDayIndex];
 
+      }//// fun. nextDay
+
+      $scope.previousDay = function(){
+        if(angular.isUndefined($scope.currentDays) || angular.isUndefined($scope.currentDayLabel) || angular.isUndefined($scope.currentDayIndex)){
+          return null;
+        }
+        var pre = ($scope.currentDayIndex - 1) % 7;
+        $scope.currentDayIndex = pre < 0 ? 6 : pre;
+        $scope.currentDayLabel = $scope.days[$scope.currentDayIndex];
+
+      }//// fun. nextDay
 
         /**
          * The way Multi step form work that you have to sabe the data in $scope destory event
