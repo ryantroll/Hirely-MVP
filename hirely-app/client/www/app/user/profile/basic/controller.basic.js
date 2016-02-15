3c579ca54d712a8512b550e2b00935bde61699df
BrianDombrowski <bdombro@gmail.com>
2016-02-15 17:03:51 -0500

    Last bugfix wasn't good.  This one works.  Also fixed dob DNE error

--- hirely-app/client/www/app/user/profile/basic/controller.basic.js
+++ hirely-app/client/www/app/user/profile/basic/controller.basic.js
@@ -235,12 +235,16 @@
         /**
          * Set scope _dateOfBirth and _mobile these 2 properites need to be fomrated before display
          */
-        if ($scope._mobile) {
+        if ($scope.user.mobile) {
           $scope._mobile = UserService.formatPhone($scope.user.mobile.split('+1.').join(''));
         } else {
           $scope._mobile = '';
         }
-        $scope._dateOfBirth = UserService.formatDate($scope.user.dateOfBirth);
+        if ($scope.user.dateOfBirth) {
+          $scope._dateOfBirth = UserService.formatDate($scope.user.dateOfBirth);
+        } else {
+          $scope._dateOfBirth = '';
+        }
 
         $scope.stepOneLoaded = true;
       }
