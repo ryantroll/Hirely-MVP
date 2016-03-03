3c7c10ace7ae5f7879204f649d3fe52c032eafef
Iyad Bitar <iyadbitar@Iyads-MacBook-Pro.local>
2016-03-03 16:14:04 -0500

    Front-end done except map

--- hirely-app/client/www/app/job/controller.job-position.js
+++ hirely-app/client/www/app/job/controller.job-position.js
@@ -36,7 +36,44 @@
         $scope.dataError = false;
       }, 200);
 
-    }
+      var sideBar = angular.element('#sideBar');
+      var stickyHeader = angular.element('#stickyHeader');
+      var posDetails = angular.element('#posDetails');
+
+      var posWindowScroll = function(e){
+         var diff = posDetails.offset().top - $(e.target).scrollTop();
+
+        if(  diff <= 0 ){
+          if( !stickyHeader.hasClass('show') ){
+            stickyHeader.addClass('show');
+          }
+
+          /**
+           * Don't make the sidebar sticky if it increase the height more than the the details height
+           * -28 because the last box in details has margin bottom and this make sidebar stop at right position
+           */
+          if(
+            sideBar.height()+Math.abs(diff) <= posDetails.height() - 28
+            && angular.element('#wageBox').css('display') !== 'none'
+          ){
+            sideBar.css('padding-top', Math.abs(diff) + 'px');
+          }
+
+        }
+        else{
+          if( stickyHeader.hasClass('show') ){
+            stickyHeader.removeClass('show');
+            sideBar.css('padding-top', '');
+          }
+        }
+
+
+        // sideBar.css('top', diff+'px');
+        // console.log(angular.element('#wageBox').css('display') );
+      }//// fun. posWindowScroll
+
+      angular.element(window).on('scroll', posWindowScroll)
+    }//// fun. initialize
   }//// fun. JobController
 
  })();
