7fa89385b00108d0c6c14cc6ec178a585bfadcc3
Iyad Bitar <iyadbitar@Iyads-MacBook-Pro.local>
2016-03-18 15:58:52 -0400

    fron-end styling done

--- hirely-app/client/www/app/business/controller.candidate-list.js
+++ hirely-app/client/www/app/business/controller.candidate-list.js
@@ -12,5 +12,86 @@
 
   function CandidateListController($scope, $stateParams, $state, DEFAULT_PROFILE_IMAGE) {
     $scope.defaultImage = DEFAULT_PROFILE_IMAGE;
+
+    $scope.showPositionMenu = false;
+    $scope.showSortMenu = false;
+    $scope.sortByLabel = 'Sort By';
+
+
+    $scope.togglePositionMenu = function(event){
+
+      var handleMenuClick = function(e){
+          if(true === $scope.showPositionMenu){
+              $scope.showPositionMenu = false;
+              $scope.$apply();
+              //// unbind when menu closed no need to check for click
+              $('body').unbind('click', handleMenuClick);
+          }
+          else{
+              $scope.showPositionMenu = true;
+              $scope.$apply();
+          }
+      };
+      /**
+       * the event will bubble up to body so do the work on body click \ only if menu is closed
+       * this to make sure the menu is closed when click outside the menu
+       */
+      if(false === $scope.showPositionMenu){
+          $('body').bind('click', handleMenuClick);
+      }
+    }//// togglePositionMenu
+
+    $scope.toggleSortMenu = function(ev){
+        var handleMenuClick = function(e){
+          if(true === $scope.showSortMenu){
+              $scope.showSortMenu = false;
+              $scope.$apply();
+              //// unbind when menu closed no need to check for click
+              $('body').unbind('click', handleMenuClick);
+          }
+          else{
+              $scope.showSortMenu = true;
+              $scope.$apply();
+          }
+      };
+      if(false === $scope.showSortMenu){
+          $('body').bind('click', handleMenuClick);
+
+          /**
+           * bind click event on list items
+           */
+          var list = angular.element('#sortMenu').find('a');
+
+          list.off('click').on('click', function(e){
+            var me = angular.element(e.currentTarget);
+            me.parent().addClass('active');
+            $scope.sortBy = me.attr('data-value');
+            $scope.sortByLabel = me.text();
+          })
+      }
+    }//// fun. showSortMenu
+
+    $scope.toggleActionMenu = function(ev, menuId){
+        var menu = angular.element('#menu_' + menuId);
+
+        var handleMenuClick = function(e){
+          if(false === menu.hasClass('hidden')){
+              // $scope.showActionMenu = false;
+              // $scope.$apply();
+              menu.addClass('hidden');
+              //// unbind when menu closed no need to check for click
+              $('body').unbind('click', handleMenuClick);
+          }
+          else{
+              // $scope.showActionMenu = true;
+              // $scope.$apply();
+              menu.removeClass('hidden');
+          }
+      };
+
+      if(true === menu.hasClass('hidden')){
+        $('body').bind('click', handleMenuClick);
+      }
+    } //// fun. toggleActionMenu
   }
 })();
--- hirely-app/client/www/app/layout/business/controller.business-master.js
+++ hirely-app/client/www/app/layout/business/controller.business-master.js
@@ -17,31 +17,7 @@
 
   function BusinessMasterController($scope, $http, $state, $stateParams) {
 
-    $scope.showPositionMenu = false;
 
-
-    $scope.togglePositionMenu = function(event){
-      console.log('menu')
-      var handleMenuClick = function(e){
-          if(true === $scope.showPositionMenu){
-              $scope.showPositionMenu = false;
-              $scope.$apply();
-              //// unbind when menu closed no need to check for click
-              $('body').unbind('click', handleMenuClick);
-          }
-          else{
-              $scope.showPositionMenu = true;
-              $scope.$apply();
-          }
-      };
-      /**
-       * the event will bubble up to body so do the work on body click \ only if menu is closed
-       * this to make sure the menu is closed when click outside the menu
-       */
-      if(false === $scope.showPositionMenu){
-          $('body').bind('click', handleMenuClick);
-      }
-    }//// togglePositionMenu
 
   }//// fun. JobController
 
