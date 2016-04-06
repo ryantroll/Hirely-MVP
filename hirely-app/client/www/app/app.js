e9bc4326166f2b442e4170c5c57bceef6ea85bba
BrianDombrowski <bdombro@gmail.com>
2016-04-06 16:46:50 -0400

    Switching prod config to express

--- hirely-app/client/www/app/app.js
+++ hirely-app/client/www/app/app.js
@@ -128,5 +128,5 @@ var myApp = angular.module('hirelyApp',
 
 
         // if none of the above states are matched, use this as the fallback
-        // $urlRouterProvider.otherwise('/');
+        $urlRouterProvider.otherwise('/');
     });
