9929049a02f05950ce1f9547a03bae556c985025
BrianDombrowski <bdombro@gmail.com>
2016-04-12 17:58:00 -0400

    Bugfix require terms

--- hirely-app/client/www/app/account/controller.register.js
+++ hirely-app/client/www/app/account/controller.register.js
@@ -5,24 +5,25 @@
     'use strict';
 
     angular.module('hirelyApp.account')
-    .directive('validatePassword', function(){
-    return {
-        // TODO:  Consider if we can use ng-pattern like we do for basic info postal code
-      restrict:'A',
-      require:'ngModel',
-      link:function(scope, ele, attrs, ctrl){
-        ctrl.$parsers.unshift(function(value){
-          // Contains at least 8 chars, 1 lowercase, 1 uppercase, and 1 number.
-          var minSatRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[0-9a-zA-Z]{8,}$/;
+        .directive('validatePassword', function () {
+            return {
+                // TODO:  Consider if we can use ng-pattern like we do for basic info postal code
+                restrict: 'A',
+                require: 'ngModel',
+                link: function (scope, ele, attrs, ctrl) {
+                    ctrl.$parsers.unshift(function (value) {
+                        // Contains at least 8 chars, 1 lowercase, 1 uppercase, and 1 number.
+                        var minSatRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[0-9a-zA-Z]{8,}$/;
 
-          // ctrl.$setValidity('invalidPassword', minSatRegex.test(value) && freeFromBannedRegex.test(value));
-            ctrl.$setValidity('invalidPassword', minSatRegex.test(value));
-          return value;
-        });/// unshift
-      }//// fun. link
-    }/// return object
-  })/// validate date;
-    .controller('RegisterController', ['$location', '$scope', '$rootScope', '$state', '$stateParams', '$timeout', 'AuthService', RegisterController ]);
+                        // ctrl.$setValidity('invalidPassword', minSatRegex.test(value) && freeFromBannedRegex.test(value));
+                        ctrl.$setValidity('invalidPassword', minSatRegex.test(value));
+                        return value;
+                    });/// unshift
+                }//// fun. link
+            }/// return object
+        })/// validate date;
+
+        .controller('RegisterController', ['$location', '$scope', '$rootScope', '$state', '$stateParams', '$timeout', 'AuthService', RegisterController]);
 
     function RegisterController($location, $scope, $rootScope, $state, $stateParams, $timeout, AuthService) {
 
@@ -36,33 +37,33 @@
 
         $scope.message = $stateParams.message;
 
-        $timeout(function() {
+        $timeout(function () {
             $(window).scrollTop(0);
         });
 
-        $scope.resetEmailValidity = function(){
-          $scope.registerForm.email.$setValidity('emailExists', true);
+        $scope.resetEmailValidity = function () {
+            $scope.registerForm.email.$setValidity('emailExists', true);
         };
 
-        $scope.handleRgisterForm = function(){
-            if(!$scope.registerForm.$valid){
+        $scope.handleRgisterForm = function () {
+            if (!$scope.registerForm.$valid) {
                 return null;
             }
             $scope.ajaxBusy = true;
             registerPasswordUser($scope.user);
         };
 
-        $scope.cancelRegistration = function() {
-            if($rootScope.nextState.length) {
+        $scope.cancelRegistration = function () {
+            if ($rootScope.nextState.length) {
                 var nextState = $rootScope.nextState.pop();
                 $state.go(nextState.state, nextState.params);
             }
-            else{
+            else {
                 $state.go('master.default.user.profile')
             }
         }
 
-        function registerPasswordUser(registeredUser){
+        function registerPasswordUser(registeredUser) {
             /**
              * Let the parent scope know it is a new regitered user
              */
@@ -73,36 +74,35 @@
             //register new user
             AuthService.registerNewUser(registeredUser)
                 .then(
-                    function(user) {
-                      if (user) {
-                          $scope.registerForm.email.$setValidity('emailExists', true);
-                          /**
-                           * Check if nextState is set in rootScope and redirect user to it
-                           */
-                          if($rootScope.nextState.length) {
-                              var nextState = $rootScope.nextState.pop();
-                              $state.go(nextState.state, nextState.params);
-                              delete $rootScope.nextState;
-                          }
-                          else{
-                              $state.go('master.default.dashboard')
-                          }
-                      } else {
-                          $scope.registerForm.email.$setValidity('emailExists', false);
-                      }
-
+                    function (user) {
+                        if (user) {
+                            $scope.registerForm.email.$setValidity('emailExists', true);
+                            /**
+                             * Check if nextState is set in rootScope and redirect user to it
+                             */
+                            if ($rootScope.nextState.length) {
+                                var nextState = $rootScope.nextState.pop();
+                                $state.go(nextState.state, nextState.params);
+                                delete $rootScope.nextState;
+                            }
+                            else {
+                                $state.go('master.default.dashboard')
+                            }
+                        } else {
+                            $scope.registerForm.email.$setValidity('emailExists', false);
+                        }
 
 
                         $scope.ajaxBusy = false;
                         return user;
-                  },
-                  function(err) {
-                      /**
-                       * User authentication couldn't be created
-                       */
-                      $scope.registerForm.email.$setValidity('emailExists', false);
-                      $scope.ajaxBusy = false;
-                  }
+                    },
+                    function (err) {
+                        /**
+                         * User authentication couldn't be created
+                         */
+                        $scope.registerForm.email.$setValidity('emailExists', false);
+                        $scope.ajaxBusy = false;
+                    }
                 )
 
 
@@ -110,7 +110,6 @@
 
 
     } // end ctrl
-
 
 
 })();
