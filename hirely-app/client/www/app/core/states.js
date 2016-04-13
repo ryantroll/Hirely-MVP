6824138167cd5a6ae8c9ca60f901d949e32dfede
BrianDombrowski <bdombro@gmail.com>
2016-04-13 08:48:35 -0400

    Redirect all apply links ending in applink.aspx to team-member/apply

--- hirely-app/client/www/app/core/states.js
+++ hirely-app/client/www/app/core/states.js
@@ -11,6 +11,7 @@
                 $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams) {
                     if (toState.name == 'master.application.applyTmp') {
                         console.warn("S:$stateChangeStart:warn:0: caught applyTmp redirect state. Redirecting.")
+                        toParams.positionSlug = 'team-member';
                         $state.go('master.application.apply', toParams);
                         event.preventDefault();
                     }
